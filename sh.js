2fce4f0a76a96715878a3978daca44d1e9919ac9
Brian Mai <0hrs3g@gmail.com>
2023-06-19 09:42:39 -0700

    edit counter logic

--- sh.js
+++ sh.js
@@ -7,7 +7,7 @@ let goal = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
 // location of empty cell [row, col]
 let empty = [-1, -1];
 
-// flag indicates when timer is running and start audio
+// flag indicates when timer is running
 let off = true;
 let flag = true;
 
@@ -62,16 +62,16 @@ window.onkeydown = e => {
     if (sfx_name == "media/s1.mp3") { sfx.volume = 1; }
     else { sfx.volume = 0.5; }
     sfx.play();
-    ++moves, moveCounter.innerText = moves;
+    ++moves, moveCounter.innerText = "Moves: " + moves;
     
-    // start timer and audio upon first move
+    // start timer upon first move
     if (off) { startTimer(), off = false; }
     if (flag) {
-        let bg = new Audio("media/bg.mp3");
-        bg.volume = 0.3;
-        bg.loop = true;
-        bg.play();
-        flag = false;
+      let bg = new Audio("media/bg.mp3");
+      bg.volume = 0.3;
+      bg.loop = true;
+      bg.play();
+      flag = false;
     }
     let i = empty[0], j = empty[1];
     if ((e.key == "ArrowUp" || e.key == "w") && empty[0] != 3) {
@@ -174,7 +174,7 @@ const shuffle = () => {
     while (!solvable(init.flat())); // holy motherfucker fucking fuck shit holy fuck i wanna die
     // console.log(init);
     stopTimer(), timerElement.innerHTML = "00:00:000", off = true;
-    moves = 0, moveCounter.innerText = 0;
+    moves = 0, moveCounter.innerText = "Moves: 0";
     renderBoard();
     colorBoard();
     // console.log("I beg you to be solvable.");

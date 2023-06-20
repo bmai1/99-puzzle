90854fe11fae59adaf147749d2e5367ac553b75b
Brian Mai <0hrs3g@gmail.com>
2023-06-19 21:38:38 -0700

    finally fixed audio player

--- sh.js
+++ sh.js
@@ -9,6 +9,7 @@ let empty = [-1, -1];
 
 // flag indicates when timer is running
 let off = true;
+// let curr_song = null;
 let flag = true;
 
 window.addEventListener("DOMContentLoaded", (event) => {
@@ -100,10 +101,11 @@ window.onkeydown = e => {
     // start timer upon first move
     if (off) { startTimer(), off = false; }
     if (flag) {
-      let bg = new Audio("media/bg.mp3");
-      bg.volume = 0.3;
-      bg.loop = true;
-      bg.play();
+      loadTrack(track_index);
+      playpauseTrack();
+      // curr_song  = new Audio("media/bg.mp3");
+      // curr_song.loop = true;
+      // curr_song.play();
       flag = false;
     }
 
@@ -390,4 +392,174 @@ const check = () => {
 const popup = document.getElementById("playAgain");
 const hide = () => {
   playAgain.style.display = "none";
+}
+
+// music player 
+let player = document.getElementById("player");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+ 
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+ 
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+ 
+// Specify globally used values
+let track_index = 0;
+let isPlaying = false;
+let songTimer;
+
+let curr_track = document.createElement('audio');
+
+let track_list = [
+  {
+    name: "shinsetsu (親切)",
+    artist: "物語シリーズ",
+    image: "media/shinsetsu.png",
+    path: "media/bg1.mp3",
+    bgColor: "rgb(240, 98, 121, 0.9)"
+  },
+  {
+    name: "campo de girassol",
+    artist: "shibayan records",
+    image: "media/cdgir.png",
+    path: "media/bg2.mp3",
+    bgColor: "rgb(36, 34, 102, 0.6)"
+  },
+  {
+    name: "garota de ipanema",
+    artist: "lisa ono",
+    image: "media/lisa.png",
+    path: "media/bg3.mp3",
+    bgColor: "rgb(71, 68, 64, 0.7)"
+  },
+];
+
+const loadTrack = (track_index) => {
+  // Clear the previous seek timer
+  clearInterval(songTimer);
+  resetValues();
+ 
+  // Load a new track
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+  // curr_track = new Audio(`${track_list[track_index].path}`);
+  // curr_track.play();
+ 
+  // Update details of the track
+  track_art.style.backgroundImage =
+     "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  player.style.backgroundColor = track_list[track_index].bgColor;
+ 
+  // Set an interval of 1000 milliseconds
+  // for updating the seek slider
+  songTimer = setInterval(seekUpdate, 1000);
+ 
+  // Move to the next track if the current finishes playing
+  // using the 'ended' event
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+resetValues = () => {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+const playpauseTrack = () => {
+  // Switch between playing and pausing
+  // depending on the current state
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+ 
+const playTrack = () => {
+  // Play the loaded track
+  curr_track.play();
+  isPlaying = true;
+ 
+  // Replace icon with the pause icon
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+ 
+const pauseTrack = () => {
+  // Pause the loaded track
+  curr_track.pause();
+  isPlaying = false;
+ 
+  // Replace icon with the play icon
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+ 
+const nextTrack = () => {
+  // Go back to the first track if the
+  // current one is the last in the track list
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+ 
+  // Load and play the new track
+  loadTrack(track_index);
+  playTrack();
+}
+ 
+const prevTrack = () => {
+  // Go back to the last track if the
+  // current one is the first in the track list
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length - 1;
+   
+  // Load and play the new track
+  loadTrack(track_index);
+  playTrack();
+}
+
+const seekTo = () => {
+  // Calculate the seek position by the
+  // percentage of the seek slider
+  // and get the relative duration to the track
+  seekto = curr_track.duration * (seek_slider.value / 100);
+ 
+  // Set the current track position to the calculated seek position
+  curr_track.currentTime = seekto;
+}
+ 
+const setVolume = () => {
+  // Set the volume according to the
+  // percentage of the volume slider set
+  curr_track.volume = volume_slider.value / 100;
+}
+ 
+const seekUpdate = () => {
+  let seekPosition = 0;
+ 
+  // Check if the current track duration is a legible number
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+ 
+    // Calculate the time left and the total duration
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+ 
+    // Add a zero to the single digit time values
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+ 
+    // Display the updated duration
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
 }
